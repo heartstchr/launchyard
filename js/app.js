b67d1e9a40bc5104589fbe7acbc4789b049a3762
jiwan ghosal <heartstchr@live.com>
2014-11-27 10:32:17 -0800

    index learn more fixed

--- js/app.js
+++ js/app.js
@@ -1,12 +1,35 @@
 App = Ember.Application.create();
 
 App.Router.map(function(){
-	this.resource('about');
-	this.resource('Case-studies');
+	this.resource('about',{path: '/about'});
+	this.resource('Case-studies',{path: '/Case-studies'});
 	this.resource('getintouch');
-	this.resource('ideationSection');
-
+	this.resource('ideationSection',{path: 'ideationSection'});
 });
+
+
+App.IndexRoute = Ember.Route.extend({
+	actions:{
+		learnMore: function(){
+			this._super();
+			window.location = "#ideation"
+		}
+	}	
+});
+
+App.CaseStudiesRoute = Ember.Route.extend({
+	actions:{
+		down: function(){
+			this._super();
+			window.location = "#down"
+		}
+	}	
+});
+
+App.IndexController = Ember.Controller.extend({
+	title: 'We help build startups.' 
+});
+
 
 App.getintouchRoute = Ember.Route.extend({
 
