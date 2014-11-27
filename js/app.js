3986ddc43a366b83f6ebd6fc9cd0b8947e0824dc
jiwan ghosal <heartstchr@live.com>
2014-11-27 15:01:48 -0800

    testinomials
    each section is translated into testinomials

--- js/app.js
+++ js/app.js
@@ -3,27 +3,40 @@ App = Ember.Application.create();
 App.Router.map(function(){
 	this.resource('about',{path: '/about'});
 	this.resource('Case-studies',{path: '/Case-studies'});
-	this.resource('getintouch');
+	this.resource('getInTouch');
 	this.resource('ideationSection',{path: 'ideationSection'});
+	this.resource('caseStudiesAbout',{path: 'caseStudiesAbout'});
 });
 
-
 App.IndexRoute = Ember.Route.extend({
-	actions:{
-		learnMore: function(){
-			this._super();
-			window.location = "#ideation"
-		}
-	}	
+	renderTemplate: function(){
+  	this.render('index',{outlet: 'index'});
+  	this.render('ideationSection',{outlet: 'ideationSection'});
+  	this.render('platformSection',{outlet: 'platformSection'});
+  	this.render('featureSection',{outlet: 'featureSection'});
+  	this.render('getInTouch',{outlet: 'getInTouch'});
+  	this.render('clientSection',{outlet: 'clientSection'});
+  	this.render('freeLandingPage',{outlet: 'freeLandingPage'});
+  	this.render('achievementSection',{outlet: 'achievementSection'});
+  }	
 });
 
 App.CaseStudiesRoute = Ember.Route.extend({
-	actions:{
-		down: function(){
-			this._super();
-			window.location = "#down"
-		}
-	}	
+	renderTemplate: function(){
+  	this.render('content',{outlet: 'content'});
+  	this.render('caseStudiesAbout',{outlet: 'caseStudiesAbout'});
+  	this.render('footer',{outlet: 'footer'});
+  	}
+});
+
+App.AboutRoute = Ember.Route.extend({
+	renderTemplate: function(){
+  	this.render('about',{outlet: 'about'});
+  	this.render('peopleSection',{outlet: 'peopleSection'});
+  	this.render('faqSection',{outlet: 'faqSection'});
+  	this.render('locationSection',{outlet: 'locationSection'});
+  	this.render('footer',{outlet:'footer'});
+  	}
 });
 
 App.IndexController = Ember.Controller.extend({
@@ -31,10 +44,13 @@ App.IndexController = Ember.Controller.extend({
 });
 
 
-App.getintouchRoute = Ember.Route.extend({
+App.GetInTouchRoute = Ember.Route.extend({
 
-	model: function(){
-		return getintouch;
+	actions:{
+		getInTouch: function(){
+			this._super();
+			window.location = "#getInTouch"
+		}
 	}
 });
 
